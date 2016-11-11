f5cf43d0e4a5e04b17e4f935522c1dcedffc437b
reneezhu <reneezhu1994@gmail.com>
2016-11-10 20:15:44 -0600

    start
    start from here

---
+++ cn/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>依霏</title>
+<style type="text/css">
+body {
+	background-image: url(../img/cn_bg.gif);
+}
+</style>
+</head>
+
+<body>
+<h1><center>昔我往矣 杨柳依依 今我来思 雨雪霏霏</center></h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<meta charset="utf-8"/>
+<html> 
+<style type="text/css">
+body {
+	background-image: url(img/home_bg.jpg);
+}
+</style>
+<head> 
+<title>Welcome to my site</title> 
+</head> 
+<body> 
+<h1><center>Welcome</center></h1> 
+<p>I'm glad you could make it. Click <a href="cn/index.php">here</a> to learn more about me.</p> 
+</body> 
+</html>
